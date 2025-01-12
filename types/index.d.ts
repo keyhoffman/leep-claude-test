ded5ef257c1e9d1a14be3fab195bbf8ed4cf9c18
Key Hoffman <key.hoffman.dev@gmail.com>
2025-01-11 23:34:30 -0600

    Add global styles, component types, and additional UI components

---
+++ types/index.d.ts
@@ -0,0 +1,13 @@
+type IconType = React.ElementType;
+
+interface FeatureCardProps {
+  icon: IconType;
+  title: string;
+  description: string;
+}
+
+interface StepCardProps {
+  number: string;
+  title: string;
+  description: string;
+}
